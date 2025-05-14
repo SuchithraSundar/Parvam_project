fa99cdf4d402acf844fd9059eb83a144511ac5bb
SuchithraSundar <suchithras.parvam@gmail.com>
2025-05-14 14:36:54 +0530

    updated code

--- DentalToolSupplySystem/src/main/java/com/dental/DentalToolSupplySystem/controller/HomeController.java
+++ DentalToolSupplySystem/src/main/java/com/dental/DentalToolSupplySystem/controller/HomeController.java
@@ -1,7 +1,9 @@
 package com.dental.DentalToolSupplySystem.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class HomeController {
 	
 	@GetMapping("/")
@@ -23,6 +25,48 @@ public class HomeController {
 	public String forgetPassword() {
 		return "forgetpassword";
 	}
+	
+	@GetMapping("/about")
+	public String  about() {
+		return "about";
+	}
+	
+	@GetMapping("/cart")
+	public String  cart() {
+		return "cart";
+	}
+	
+	@GetMapping("/categories")
+	public String  categories() {
+		return "categories";
+	}
+	
+	@GetMapping("/checkouts")
+	public String  checkouts() {
+		return "checkouts";
+	}
+	
+	@GetMapping("/contact")
+	public String  contact() {
+		return "contact";
+	}
+	
+	@GetMapping("/products")
+	public String  products() {
+		return "products";
+	}
+	
+	@GetMapping("/terms_conditions")
+	public String  terms_conditions() {
+		return "terms_conditions";
+	}
+	
+	
+	
+	
+	
+	
+	
 
 }
 
